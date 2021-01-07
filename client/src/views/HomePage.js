7274829d919fc6ebcc7534e3cf2b456df6215672
Legion <pawel.kapusta@student.uj.edu.pl>
2021-01-07 22:39:01 +0100

    fix: homePage, itemCards, routes

--- client/src/views/HomePage.js
+++ client/src/views/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import ImageCarousel from '../components/atoms/ImageSlider/ImageCarousel';
-import { CarouselData } from '../components/atoms/ImageSlider/CarouselData';
 import HomeCard from '../components/atoms/HomeCard/HomeCard';
+import Heading from '../components/atoms/Heading/Heading';
 
 const CardsSection = styled.div`
   margin: auto;
@@ -18,34 +20,52 @@ const Row = styled.div`
   table-layout: fixed;
   border-spacing: 10px;
 `;
+const Newest = styled(Heading)`
+  margin-top: 2%;
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 45px;
+  font-weight: 600;
+  background: linear-gradient(to top left, #66ffff 0%, #6699ff 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+const HomePage = ({ products }) => {
+  const productsSortedByQuantity = products.sort((a, b) => a.quantity - b.quantity).slice(0, 3);
+  const productsSortedByID = products.sort((a, b) => b.id - a.id).slice(0, 3);
+  return (
+    <CardsSection>
+      <ImageCarousel slides={productsSortedByQuantity} />;<Newest>New items</Newest>
+      <Row>
+        {productsSortedByID.map(({ image, name, price, id }) => (
+          <Column>
+            <HomeCard image={image} name={name} price={price} id={id} />
+          </Column>
+        ))}
+      </Row>
+    </CardsSection>
+  );
+};
 
-const HomePage = () => (
-  <CardsSection>
-    <ImageCarousel slides={CarouselData} />;
-    <Row>
-      <Column>
-        <HomeCard
-          image="https://images.unsplash.com/photo-1550520920-27ba45c38740?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-          name="Computer"
-          price="1345,34 zł"
-        />
-      </Column>
-      <Column>
-        <HomeCard
-          image="https://images.unsplash.com/photo-1550520920-27ba45c38740?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-          name="Computer"
-          price="1345,34 zł"
-        />
-      </Column>
-      <Column>
-        <HomeCard
-          image="https://images.unsplash.com/photo-1550520920-27ba45c38740?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80g"
-          name="Computer"
-          price="1345,34 zł"
-        />
-      </Column>
-    </Row>
-  </CardsSection>
-);
+HomePage.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      quantity: PropTypes.number.isRequired,
+    }),
+  ),
+};
 
-export default HomePage;
+HomePage.defaultProps = {
+  products: [],
+};
+
+const mapStateToProps = (state) => {
+  const { products } = state;
+  return { products };
+};
+
+export default connect(mapStateToProps)(HomePage);
