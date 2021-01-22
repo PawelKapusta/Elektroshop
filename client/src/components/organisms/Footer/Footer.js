a7ca3b83afda53dacc8f00854139232fed1597b9
Jakub Sroka <synmlynarza1@gmail.com>
2021-01-22 17:18:38 +0100

    fix colors

--- client/src/components/organisms/Footer/Footer.js
+++ client/src/components/organisms/Footer/Footer.js
@@ -3,24 +3,27 @@ import styled from 'styled-components';
 import Logo2 from '../../../assets/images/Logo2.png';
 
 export const Foot = styled.div`
-  bottom: 0;
+  margin-top :10px;
+  bottom: 0px;
   width: 100%;
   height: 100px;
   background: #f2709c;
-  background: -webkit-linear-gradient(to right, #ff9472, #f2709c);
-  background: linear-gradient(to right, #ff9472, #f2709c);
+  padding-top: 20px;
+ // background: -webkit-linear-gradient(to right, #ff9472, #f2709c);
+  background: #454141;
   font-weight: 500;
   font-size: 1.4rem;
   text-align: center;
   float: bottom;
+
 `;
 
 export const Footimg = styled.div`
-  margin: 0 auto;
-  padding: 0 auto;
+  margin: 0px   auto;
+  padding: 0px auto;
   display: block;
   left: 50%;
-  bottom: 0px;
+  top: 0px;
 `;
 
 export const Foottext = styled.div`
@@ -28,7 +31,8 @@ export const Foottext = styled.div`
   padding: 0 auto;
   display: block;
   left: 50%;
-  bottom: 0px;
+  top: 0px;
+  font-size: 1.4rem;    
   text-align: center;
 `;
 
@@ -38,7 +42,7 @@ const Footer = () => (
       <Footimg>
         <img src={Logo2} alt="LogoImage" />
       </Footimg>
-      Copyright ©2020 All rights reserved{' '}
+      <Foottext>Copyright ©2020 All rights reserved{' '}</Foottext>
     </Foot>
   </>
 );
